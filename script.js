c7224a5617b220c95ef2e63aa917a390a3a34ef0
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-10 12:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your Script here.
+function rot13(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    let ascii = str.charCodeAt(i);
+    if (ascii >= 65 && ascii <= 90) {
+      ascii = ((ascii - 65 + 13) % 26) + 65;
+    }
+    result += String.fromCharCode(ascii);
+  }
+  return result;
+}
